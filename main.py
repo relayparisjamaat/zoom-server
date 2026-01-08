6b86a1f5448ad3e7391fdc2ddbfeb82691164aa7
relayparisjamaat <relay.parisjamaat@gmail.com>
2026-01-08 16:02:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from fastapi import FastAPI, Request, HTTPException
-
-app = FastAPI()
-
-JOTFORM_SECRET = "515253"
-
-@app.get("/")
-def root():
-    return {"status": "server running"}
-
-@app.get("/jotform")
-def jotform_test():
-    return {"status": "jotform endpoint ready"}
-
 @app.post("/jotform")
 async def jotform_webhook(request: Request):
     data = await request.form()
-
-    # 🔐 Vérification du secret
-    if data.get("secret") != JOTFORM_SECRET:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-
-    print("✅ Webhook sécurisé reçu :", data)
+    print("📦 DATA BRUTE JOTFORM :", data)
     return {"status": "received"}
