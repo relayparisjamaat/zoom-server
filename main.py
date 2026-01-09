0eb40dcbc13cee1c7eeeda2fd9a064f660c2b1a0
relayparisjamaat <relay.parisjamaat@gmail.com>
2026-01-09 20:07:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def jotform_webhook(request: Request):
     try:
         form = await request.form()
         raw = form.get("rawRequest")
-
+u
         if not raw:
             raise HTTPException(status_code=400, detail="rawRequest manquant")
 
@@ -115,7 +115,7 @@ async def jotform_webhook(request: Request):
             "duration": duration,
             "agenda": description,
             "settings": {
-                "alternative_hosts": "", #email,
+                "alternative_hosts": email,
                 "auto_recording": "cloud" if recording else "none"
             }
         }
@@ -170,6 +170,7 @@ Vous êtes désigné comme co-hôte de la réunion.
 @app.get("/")
 def root():
     return {"status": "server running"}
+
 
 
 
